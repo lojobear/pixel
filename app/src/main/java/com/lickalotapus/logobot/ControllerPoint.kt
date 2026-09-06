ffcc9b41df5a566d613b17e7330a365afcf2185a
lojobear <logaandavid@gmail.com>
2026-09-06 19:23:24 -0400

    Add controller calibration points

---
+++ app/src/main/java/com/lickalotapus/logobot/ControllerPoint.kt
@@ -0,0 +1,18 @@
+package com.lickalotapus.logobot
+
+enum class ControllerPoint(val label: String) {
+    X("X / Cross"),
+    CIRCLE("Circle"),
+    SQUARE("Square"),
+    TRIANGLE("Triangle"),
+    DPAD_UP("D-pad Up"),
+    DPAD_DOWN("D-pad Down"),
+    DPAD_LEFT("D-pad Left"),
+    DPAD_RIGHT("D-pad Right"),
+    L1("L1"),
+    R1("R1"),
+    L2("L2"),
+    R2("R2"),
+    LEFT_STICK("Left stick center"),
+    RIGHT_STICK("Right stick center")
+}
